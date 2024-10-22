260a57516359e3bd29c4d2385c82d14b0a425891
Aek <ekkawin.v@gmail.com>
2024-10-22 19:13:41 +1100

    chore: see the log

--- apps/platform/src/index.ts
+++ apps/platform/src/index.ts
@@ -148,6 +148,7 @@ app.post("/score", async (req, res) => {
       }
     }
     if (!logs?.length) throw new Error("no reccord");
+    console.log(logs)
 
     const hasClaimed = await prisma.event.findUnique({
       where: { eventNumber: Number(eventNumber) },
